f132bbf21e77f0b6e664d23a45b286f820e4fc44
Loik Galtier <loik2galtier@gmail.com>
2025-03-14 08:52:56 +0100

    Adapted i think

--- Script/TakuzuRules.cpp
+++ Script/TakuzuRules.cpp
@@ -1,46 +1,49 @@
 #include <iostream>
 #include <math.h>
 
+#include <Rcpp.h> // To interface with our R program;
+using namespace Rcpp;
+
 //size of the TrueGrid
-#define SIZE 16 //need always pair
-int TrueGrid[SIZE][SIZE];
-int ActualGrid[SIZE][SIZE];
-int HiddenGrid[SIZE][SIZE];
+#define SIZE 12 //need always pair
+NumericMatrix TrueGrid(SIZE, SIZE);
+NumericMatrix ActualGrid(SIZE, SIZE);
+NumericMatrix HiddenGrid(SIZE, SIZE);
 
 // rules
-bool isValidLine(int line, int Grid[SIZE][SIZE]){
+bool isValidLine(int line, NumericMatrix &Grid){
     int count0 = 0;
     int count1 = 0;
     for(int i = 0; i < SIZE; i++){
-        if(Grid[line][i] == 0){
+        if(Grid(line, i) == 0){
             count0++;
-        }else if(Grid[line][i] == 1){
+        }else if(Grid(line, i) == 1){
             count1++;
         }
     }
     return count0 == count1;
 }
 
-bool isValidCol(int col,  int Grid[SIZE][SIZE]){
+bool isValidCol(int col, NumericMatrix &Grid){
     int count0 = 0;
     int count1 = 0;
     for(int i = 0; i < SIZE; i++){
-        if(Grid[i][col] == 0){
+        if(Grid(line, i) == 0){
             count0++;
-        }else if(Grid[i][col] == 1){
+        }else if(Grid(line, i) == 1){
             count1++;
         }
     }
     return count0 == count1;
 }
 
-bool isValidBoard( int Grid[SIZE][SIZE]){
+bool isValidBoard(NumericMatrix &Grid){
     for(int i = 0; i < SIZE; i++){
         for(int j = 0; j < SIZE; j++){
-            if(i > 1 && Grid[i][j] == Grid[i-1][j] && Grid[i][j] == Grid[i-2][j] && Grid[i][j] != -1){
+            if(i > 1 && Grid(i, j) == Grid(i-1, j) && Grid(i, j) == Grid(i-2, j) && Grid(i, j) != -1){
                 return false;
             }
-            if(j > 1 && Grid[i][j] == Grid[i][j-1] && Grid[i][j] == Grid[i][j-2] && Grid[i][j] != -1){
+            if(j > 1 && Grid(i, j) == Grid(i, j-1) && Grid(i, j) == Grid(i, j-2) && Grid(i, j) != -1){
                 return false;
             }
         }
@@ -48,26 +51,28 @@ bool isValidBoard( int Grid[SIZE][SIZE]){
     return true;
 }
 
-bool isValiCase( int Grid[SIZE][SIZE]){
+bool isValiCase(NumericMatrix &Grid){
     for(int i = 0; i < SIZE; i++){
         for(int j = 0; j < SIZE; j++){
-            if(i > 1 && Grid[i][j] == Grid[i-1][j] && Grid[i][j] == Grid[i-2][j] && Grid[i][j] != -1){
+            if(i > 1 && Grid(i, j) == Grid(i-1, j) && Grid(i, j) == Grid(i-2, j) && Grid(i, j) != -1){
                 return false;
             }
-            if(j > 1 && Grid[i][j] == Grid[i][j-1] && Grid[i][j] == Grid[i][j-2] && Grid[i][j] != -1){
+            if(j > 1 && Grid(i, j) == Grid(i, j-1) && Grid(i, j) == Grid(i, j-2) && Grid(i, j) != -1){
                 return false;
             }
         }
     }
     return true;
 }
+
+
 //need to check that never two columns are the same
-bool isValid(int Grid[SIZE][SIZE]){
+bool isValid(NumericMatrix &Grid){
     for(int i = 0; i < SIZE; i++){
         for(int j = i + 1; j < SIZE; j++){
             bool same = true;
             for(int k = 0; k < SIZE; k++){
-                if(Grid[k][i] != Grid[k][j]){
+                if(Grid(k, i) != Grid(k, j)){
                     same = false;
                     break;
                 }
@@ -82,7 +87,7 @@ bool isValid(int Grid[SIZE][SIZE]){
         for(int j = i + 1; j < SIZE; j++){
             bool same = true;
             for(int k = 0; k < SIZE; k++){
-                if(Grid[i][k] != Grid[j][k]){
+                if(Grid(i, k) != Grid(j, k)){
                     same = false;
                     break;
                 }
@@ -107,22 +112,22 @@ void generateValidBoard(){
             int count1 = 0;
             for(int j = 0; j < SIZE; j++){
                 if(count0 < SIZE / 2 && count1 < SIZE / 2){
-                    TrueGrid[i][j] = rand() % 2;
-                    if (i > 1 && TrueGrid[i][j] == TrueGrid[i-1][j] && TrueGrid[i][j] == TrueGrid[i-2][j] && TrueGrid[i][j] != -1)
+                    TrueGrid(i,j) = rand() % 2;
+                    if (i > 1 && TrueGrid(i,j) == TrueGrid(i-1,j) && TrueGrid(i,j) == TrueGrid(i-2,j) && TrueGrid(i,j) != -1)
                     {
-                        TrueGrid[i][j] = 1 - TrueGrid[i][j];
+                        TrueGrid(i,j) = 1 - TrueGrid(i,j);
                     }
-                    if (j > 1 && TrueGrid[i][j] == TrueGrid[i][j-1] && TrueGrid[i][j] == TrueGrid[i][j-2] && TrueGrid[i][j] != -1)
+                    if (j > 1 && TrueGrid(i,j) == TrueGrid(i,j-1) && TrueGrid(i,j) == TrueGrid(i,j-2) && TrueGrid(i,j) != -1)
                     {
-                        TrueGrid[i][j] = 1 - TrueGrid[i][j];
+                        TrueGrid(i,j) = 1 - TrueGrid(i,j);
                     }
                     
                 } else if(count0 < SIZE / 2){
-                    TrueGrid[i][j] = 0;
+                    TrueGrid(i,j) = 0;
                 } else {
-                    TrueGrid[i][j] = 1;
+                    TrueGrid(i,j) = 1;
                 }
-                if(TrueGrid[i][j] == 0){
+                if(TrueGrid(i,j) == 0){
                     count0++;
                 } else {
                     count1++;
@@ -134,7 +139,7 @@ void generateValidBoard(){
     std::cout << "Board generated" << std::endl;
     for(int i = 0; i < SIZE; i++){
         for(int j = 0; j < SIZE; j++){
-            std::cout << TrueGrid[i][j] << " ";
+            std::cout << TrueGrid(i,j) << " ";
         }
         std::cout << std::endl;
     }
@@ -143,50 +148,50 @@ void generateValidBoard(){
 
 
 // clone the board
-void cloneBoard(int TrueGrid[SIZE][SIZE], int ActualGrid[SIZE][SIZE]){
+void cloneBoard(NumericMatrix &PastGrid, NumericMatrix &NewGrid){
     for(int i = 0; i < SIZE; i++){
         for(int j = 0; j < SIZE; j++){
-            ActualGrid[i][j] = TrueGrid[i][j];
+            NewGrid(i,j) = PastGrid(i,j);
         }
     }
 }
 
 // remove value (to a 7) if the value here cannot be change (only one possibility of completiun) (but not working because next ca be also change so need to check taht latter)
-void removeValue(int Grid[SIZE][SIZE], int i, int j){
-    int tmp = TrueGrid[i][j];
+void removeValue(NumericMatrix &Grid, int i, int j){
+    int tmp = TrueGrid(i,j);
     int count = 0;
     for(int k = 0; k < 2; k++){
-        TrueGrid[i][j] = k;
+        TrueGrid(i,j) = k;
         if(isValid(TrueGrid)){
             count++;
         }
 
-        if (Grid[i-1][j] == 7 && Grid[i-2][j] == 7)
+        if (Grid(i-1, j) == 7 && Grid(i-2,j) == 7)
         {
             count++;
         }
 
-        if (Grid[i][j-1] == 7 && Grid[i][j-2] == 7)
+        if (Grid(i, j-1) == 7 && Grid(i, j-2) == 7)
         {
             count++;
         }
         
     }
-    TrueGrid[i][j] = tmp; // restore the original value
+    TrueGrid(i,j) = tmp; // restore the original value
     if(count == 1){
-        Grid[i][j] = 7;
+        Grid(i, j) = 7;
     }
 }
 
 // print board enter in parameter of the void
-void printBoard(int Grid[SIZE][SIZE]){
+void printBoard(NumericMatrix &Grid){
     for(int i = 0; i < SIZE; i++){
         for(int j = 0; j < SIZE; j++){
-            if(Grid[i][j] == 1)
+            if(Grid(i, j) == 1)
             {
                 std::cout << "X ";
             }
-            else if(Grid[i][j] == 0)
+            else if(Grid(i, j) == 0)
             {
                 std::cout << "O ";
             }
@@ -201,7 +206,7 @@ void printBoard(int Grid[SIZE][SIZE]){
     std::cout << std::endl;
 }
 
-void changeValue(int Grid[SIZE][SIZE],int iteration){
+void changeValue(NumericMatrix &Grid,int iteration){
     if (iteration > SIZE * SIZE * SIZE * SIZE)
     {
         std::cout << "you don't have luck bro" << std::endl;
@@ -210,8 +215,8 @@ void changeValue(int Grid[SIZE][SIZE],int iteration){
 
     int i = rand() % SIZE;
     int j = rand() % SIZE;
-    if(Grid[i][j] != 0 && Grid[i][j] != 1){
-        Grid[i][j] = TrueGrid[i][j];
+    if(Grid(i, j) != 0 && Grid(i, j) != 1){
+        Grid(i, j) = TrueGrid(i,j);
         return;
     }
     else
